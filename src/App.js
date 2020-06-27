28b0a4330a792925773d9fa12abcfe2ade28ae07
takatoh <takatoh.m@gmail.com>
2020-06-27 12:39:50 +0900

    src/App.js: Make App be class.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Astrud
-        </p>
-      </header>
-      <main>
-        <p>
-          Photos here.
-        </p>
-      </main>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      endpoint: "http://localhost:8080",
+      photos: []
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Astrud
+          </p>
+        </header>
+        <main>
+          <p>
+            Photos here.
+          </p>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
