c288a02b3098e96c96189880ab49486001e82e46
takatoh <takatoh.m@gmail.com>
2020-07-05 07:06:44 +0900

    Fix styles for Paper and Photo.

--- src/Photo.js
+++ src/Photo.js
@@ -4,15 +4,17 @@ import './Photo.css';
 
 export default function Photo(props) {
   return (
-    <figure>
-      <a
-        href={props.photo}
-        target="_blank"
-        rel="noopener noreferrer"
-        key={props.filename}
-      >
-        <img src={props.thumbnail} alt={props.filename} />
-      </a>
-    </figure>
+    <div className="Photo-box">
+      <figure>
+        <a
+          href={props.photo}
+          target="_blank"
+          rel="noopener noreferrer"
+          key={props.filename}
+        >
+          <img src={props.thumbnail} alt={props.filename} />
+        </a>
+      </figure>
+    </div>
   );
 }
