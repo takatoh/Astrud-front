0018e9e75af6853216e641c2ff574bb0507b4d20
takatoh <takatoh.m@gmail.com>
2020-07-04 21:32:18 +0900

    src/Photo.js: Rewrite to function component.

--- src/Photo.js
+++ src/Photo.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './Photo.css';
 
 
-class Photo extends React.Component {
-  render() {
-    return (
-      <figure>
-        <a href={this.props.photo} target="_blank" key={this.props.filename} >
-          <img src={this.props.thumbnail} alt={this.props.filename} />
-        </a>
-      </figure>
-    );
-  }
+export default function Photo(props) {
+  return (
+    <figure>
+      <a
+        href={props.photo}
+        target="_blank"
+        rel="noopener noreferrer"
+        key={props.filename}
+      >
+        <img src={props.thumbnail} alt={props.filename} />
+      </a>
+    </figure>
+  );
 }
-
-
-export default Photo;
