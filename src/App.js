370a223668f8b9fa885126d5a98195884596f1b3
takatoh <takatoh.m@gmail.com>
2020-07-04 17:22:18 +0900

    src/App.js: Put AppBar instead of header.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import './App.css';
 import Tree from './Tree';
 import Paper from './Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 
 class App extends React.Component {
@@ -10,18 +26,24 @@ class App extends React.Component {
     this.state = {
       endpoint: "http://localhost:8008",
       tree: {name: "", path: "", children: []},
-      photos: []
+      photos: [],
+      classes: makeStyles(),
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Astrud
-          </p>
-        </header>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={this.state.classes.nemuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={this.state.classes.title}>
+              Astrud
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <main>
           <Tree
             root={this.state.tree}
