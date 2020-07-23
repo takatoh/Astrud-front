37d617960098f790cce624f29ee1a8579e845f41
takatoh <takatoh.m@gmail.com>
2020-07-23 11:17:05 +0900

    src/Paper.js: Tweak.

--- src/Paper.js
+++ src/Paper.js
@@ -6,20 +6,20 @@ import { Container, Typography, Breadcrumbs } from '@material-ui/core';
 
 const renderPhotos = (photos, endpoint) => {
   return (
-    photos.map(photo => (
+    photos.map(photo =>
       <Photo
         photo={`${endpoint}/${photo.photo}`}
         thumbnail={`${endpoint}/${photo.thumbnail}`}
         filename={photo.filename}
         key={photo.filename}
       />
-    ))
+    )
   );
 }
 
 const renderBreadcrumbs = (path) => {
   const ancestors = path.split("/");
-  return ancestors.map(text => <Typography>{text}</Typography>);
+  return ancestors.map(text => <Typography key={text}>{text}</Typography>);
 }
 
 export default function Paper(props) {
@@ -39,6 +39,6 @@ export default function Paper(props) {
       <div className="Paper Paper-empty">
         <p>No photos</p>
       </div>
-    )
+    );
   }
 }
