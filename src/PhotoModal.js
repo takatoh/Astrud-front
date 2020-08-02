665528727b394f9fdad72cca931f6a1dcc6ba1bd
takatoh <takatoh.m@gmail.com>
2020-08-02 16:28:33 +0900

    src/PhotoModal.js: Remove unnecessary codes.

--- src/PhotoModal.js
+++ src/PhotoModal.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import Container from '@material-ui/core/Container';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 
