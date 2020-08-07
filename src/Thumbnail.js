6eaf17bf921eb78c3651f70b33b8c80d66d68f8a
takatoh <takatoh.m@gmail.com>
2020-08-08 05:44:05 +0900

    src/Thumbnail.js: Not import 'Thumbnail.css', use 'useStyles' hook instead.

--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import './Thumbnail.css';
+import { makeStyles } from '@material-ui/core/styles';
 
 
 const Thumbnail = (props) => {
+  const classes = useStyles();
+
   return (
-    <div className="Photo-box">
-      <figure>
+    <div className={classes.box}>
+      <figure className={classes.figure}>
         <img
           src={props.thumbnail}
           alt={props.filename}
           onClick={() => props.handleClick(props.photo)}
+          className={classes.img}
         />
       </figure>
     </div>
   );
 }
+
+const useStyles = makeStyles({
+  box: {
+    float: 'left',
+    textAlign: 'center',
+  },
+  figure: {
+    height: 200,
+    width: 200,
+    display: 'table-cell',
+    verticalAlign: 'middle',
+  },
+  img: {
+    border: 'solid 1px #dddddd',
+  }
+});
 
 
 export default Thumbnail
